d4c29643ed2226ab3ad6f33dffafbaeb4c18916e
dawidzurawski <dawidzurawskidrz@gmail.com>
2022-05-24 08:42:30 +0100

    added boxes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,19 @@
 import React from "react";
+import Box from "./Box";
+import boxes from "./boxes"
 
 export default function App(){
   
+  const [squares, squareElements] = React.useState(boxes)
+  
+  squareElements = squares.map(square => (
+    <Box key = {square.id}/>
+  ))
+
+  return(
+    <div>
+      {squareElements}
+    </div>
+  )
+
 }
