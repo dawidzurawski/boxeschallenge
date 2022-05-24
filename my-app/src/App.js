6093f7c032fe80fed028052909a261042cf4601b
dawidzurawski <dawidzurawskidrz@gmail.com>
2022-05-24 10:44:11 +0100

    boxes change color through unified state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,9 +5,33 @@ import boxes from "./boxes"
 export default function App(){
   
   const [squares, setSquares] = React.useState(boxes)
-  
+
+  function toggle(id){
+    setSquares(prevSquares => {
+      const newSquares = []
+      for(let i = 0; i<prevSquares.length; i++){
+        const currentSquare = prevSquares[i]
+        if(currentSquare.id === id) {
+          const updatedSquare = {
+            ...currentSquare,
+            on: !currentSquare.on
+          }
+          newSquares.push(updatedSquare)
+        } else {
+          newSquares.push(currentSquare)
+        }
+      }
+      return newSquares
+    })
+  }
+
   const squareElements = squares.map(square => (
-    <Box key={square.id} on={square.on}/>
+    <Box 
+      key={square.id} 
+      id={square.id} 
+      on={square.on} 
+      toggle={toggle}
+    />
   ))
 
   return(
--- my-app/src/Box.js
+++ my-app/src/Box.js
@@ -1,17 +1,14 @@
 import React from "react";
 
 export default function Box(props){
-  const [mode, setMode] = React.useState(props.on)
-
-  function toggle(){
-    setMode(!mode)
-  }
 
   const styles = {
-    backgroundColor: mode ? "black" : "white"
+    backgroundColor: props.on ? "black" : "white"           
   }
 
+
+
   return (
-    <div className="box" style={styles} onClick={toggle}></div>
+    <div className="box" style={styles} onClick={()=>props.toggle(props.id)}></div>
   )
 }
